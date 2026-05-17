1bb91b3f0749497cc04e5bc48eaf48604dd84801
shanto070 <shanto281975@gmail.com>
2026-05-18 03:23:26 +0600

    common  pushed

---
+++ backend/src/common/guards/roles.guard.ts
@@ -0,0 +1,32 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { UserRole } from '../enums/user-role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user as { role?: UserRole } | undefined;
+    if (!user?.role || !requiredRoles.includes(user.role)) {
+      throw new ForbiddenException('Insufficient role permission');
+    }
+    return true;
+  }
+}
